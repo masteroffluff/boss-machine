f05637053bde6dde3068ef106deb42e478695d4b
masteroffluff <chris.chapman@linuxmail.org>
2023-11-27 09:01:01 +0000

    ideas api complete

--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionRouter = require('./minions')
+
 const   
     {createMeeting,
     getAllFromDatabase,
@@ -13,65 +15,63 @@ const
 
 module.exports = apiRouter;
 
-// /api/minions
-const minionRouter = express.Router();
-apiRouter.use('/minions',minionRouter)  
-// GET /api/minions to get an array of all minions.
-minionRouter.get('/',(req, res, next) => {
-    res.send(getAllFromDatabase('minions'))
-});
-// POST /api/minions to create a new minion and save it to the database.
-minionRouter.post('/',(req, res, next) => {
-    const newMinion = addToDatabase('minions',req.body)
-    if (!newMinion){
-        let newMinionError =  new Error('Minion creation failed!');
-        newMinionError.status=403;
-        return next(newMinionError);
-    }
-    res.status(201).send(newMinion);
+apiRouter.use('/minions',minionRouter);
+
+
+// /api/ideas
+const ideaRouter = express.Router();
+apiRouter.use('/ideas',ideaRouter);
+
+// GET /api/ideas to get an array of all ideas.
+ideaRouter.get('/',(req, res, next) => {
+    res.send(getAllFromDatabase('ideas'))
 });
 
-// summon a minion based on id or throw an error 
-minionRouter.param('minionId',(req, res, next, id) => {
-    const minion = getFromDatabaseById('minions',id)
-    if (!minion){
-        let minionError =  new Error('Minion not found!');
-        minionError.status = 404;
-        return next(minionError);
+
+// POST /api/ideas to create a new idea and save it to the database.
+const checkMillionDollarIdea = require('./checkMillionDollarIdea')
+ideaRouter.post('/',checkMillionDollarIdea,(req, res, next) => {
+    const newIdea = addToDatabase('ideas',req.body)
+    if (!newIdea){
+        let newIdeaError =  new Error('Idea creation failed!');
+        newIdeaError.status=403;
+        return next(newIdeaError);
     }
-    req.minion = minion;
-    req.minionId= id;
+    res.status(201).send(newIdea);
+});
+
+ideaRouter.param('ideaId',(req, res, next, id) => {
+    const idea = getFromDatabaseById('ideas',id)
+    if (!idea){
+        let ideaError =  new Error('Idea not found!');
+        ideaError.status = 404;
+        return next(ideaError);
+    }
+    req.idea = idea;
+    req.ideaId= id;
     next();
 });
-// GET /api/minions/:minionId to get a single minion by id.
-minionRouter.get('/:minionId',(req, res, next) => {
-    res.send(req.minion);
-});
-// PUT /api/minions/:minionId to update a single minion by id.
-minionRouter.put('/:minionId',(req, res, next) => {
-    const newMinion = updateInstanceInDatabase('minions',{...req.minion,...req.body})
-    res.send(newMinion);
-});
 
-// DELETE /api/minions/:minionId to delete a single minion by id.
-minionRouter.delete('/:minionId',(req, res, next) =>{
-    if(deleteFromDatabasebyId('minions',req.minionId)){
+// GET /api/ideas/:ideaId to get a single idea by id.
+ideaRouter.get('/:ideaId',(req, res, next) => {
+    res.send(req.idea);
+});
+// PUT /api/ideas/:ideaId to update a single idea by id.
+// PUT /api/minions/:minionId to update a single minion by id.
+ideaRouter.put('/:ideaId',(req, res, next) => {
+    const newIdea = updateInstanceInDatabase('ideas',{...req.idea,...req.body})
+    res.send(newIdea);
+});
+// DELETE /api/ideas/:ideaId to delete a single idea by id.
+ideaRouter.delete('/:ideaId',(req, res, next) =>{
+    if(deleteFromDatabasebyId('ideas',req.ideaId)){
         res.status(204).send();
         return;
     }
-    let minionError =  new Error('Failed to delete Minion!');
-    return next(minionError);
+    let ideaError =  new Error('Failed to delete Minion!');
+    return next(ideaError);
 
 })
-
-// /api/ideas
-
-// GET /api/ideas to get an array of all ideas.
-
-// POST /api/ideas to create a new idea and save it to the database.
-// GET /api/ideas/:ideaId to get a single idea by id.
-// PUT /api/ideas/:ideaId to update a single idea by id.
-// DELETE /api/ideas/:ideaId to delete a single idea by id.
 
 // /api/meetings
 
--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,15 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) =>{
+    const {numWeeks,weeklyRevenue} = req.body;
+    
+    totalRevenue = Number(numWeeks)*Number(weeklyRevenue);
+
+    if(!totalRevenue||totalRevenue<1000000){
+        res.status(400).send("Idea does not generate a milion dollars!")
+        return
+    }
+    next();
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
+
