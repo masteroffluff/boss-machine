5fc95f85bea96268db721921821847ab8488c9d1
masteroffluff <chris.chapman@linuxmail.org>
2023-11-27 00:51:49 +0000

    minions done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 module.exports = app;
 
@@ -9,17 +10,29 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+var cors = require('cors');
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+app.use('/api',apiRouter)
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  console.log(status)
+  res.status(status).send(err.message);
+  return;
+ });
+ 
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+   });
+   
 }
--- server/api.js
+++ server/api.js
@@ -1,6 +1,82 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-
+const   
+    {createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase
+    } = require('./db')
 
 module.exports = apiRouter;
+
+// /api/minions
+const minionRouter = express.Router();
+apiRouter.use('/minions',minionRouter)  
+// GET /api/minions to get an array of all minions.
+minionRouter.get('/',(req, res, next) => {
+    res.send(getAllFromDatabase('minions'))
+});
+// POST /api/minions to create a new minion and save it to the database.
+minionRouter.post('/',(req, res, next) => {
+    const newMinion = addToDatabase('minions',req.body)
+    if (!newMinion){
+        let newMinionError =  new Error('Minion creation failed!');
+        newMinionError.status=403;
+        return next(newMinionError);
+    }
+    res.status(201).send(newMinion);
+});
+
+// summon a minion based on id or throw an error 
+minionRouter.param('minionId',(req, res, next, id) => {
+    const minion = getFromDatabaseById('minions',id)
+    if (!minion){
+        let minionError =  new Error('Minion not found!');
+        minionError.status = 404;
+        return next(minionError);
+    }
+    req.minion = minion;
+    req.minionId= id;
+    next();
+});
+// GET /api/minions/:minionId to get a single minion by id.
+minionRouter.get('/:minionId',(req, res, next) => {
+    res.send(req.minion);
+});
+// PUT /api/minions/:minionId to update a single minion by id.
+minionRouter.put('/:minionId',(req, res, next) => {
+    const newMinion = updateInstanceInDatabase('minions',{...req.minion,...req.body})
+    res.send(newMinion);
+});
+
+// DELETE /api/minions/:minionId to delete a single minion by id.
+minionRouter.delete('/:minionId',(req, res, next) =>{
+    if(deleteFromDatabasebyId('minions',req.minionId)){
+        res.status(204).send();
+        return;
+    }
+    let minionError =  new Error('Failed to delete Minion!');
+    return next(minionError);
+
+})
+
+// /api/ideas
+
+// GET /api/ideas to get an array of all ideas.
+
+// POST /api/ideas to create a new idea and save it to the database.
+// GET /api/ideas/:ideaId to get a single idea by id.
+// PUT /api/ideas/:ideaId to update a single idea by id.
+// DELETE /api/ideas/:ideaId to delete a single idea by id.
+
+// /api/meetings
+
+// GET /api/meetings to get an array of all meetings.
+// POST /api/meetings to create a new meeting and save it to the database.
+// DELETE /api/meetings to delete all meetings from the database.
+
+
